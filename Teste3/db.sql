2ce927f814400384ab07e4bd9c76e8a3293c1da9
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-05-27 09:55:16 -0400

    Update db.sql

--- Teste3/db.sql
+++ Teste3/db.sql
@@ -1,29 +1,19 @@
+create database teste3;
+
 use teste3;
 
 create table relatorio_cadop(
-	registro_ans bigint primary key,
-    cnpj varchar(255),
-    razao_social varchar(500),
-    nome_fantasia varchar(255),
-    modalidade varchar(255),
-    logradouro varchar(255),
-    numero varchar(255),
-    complemento varchar(255),
-    bairro varchar(255),
-    cidade varchar(255),
-    uf char(3),
-    cep bigint,
-    ddd int,
-    telefone bigint,
-    fax bigint,
-    endereco_eletronico varchar(255),
-    representante varchar(255),
-    cargo varchar(255),
-    data_registro_ans  varchar(10)
+	data_registro_ans  varchar(10),
+	reg_ans bigint,
+	cd_conta_contabil bigint,
+   	descricao varchar(255),
+	vl_saldo_final varchar(255)
 );
 
-load data local infile 'Documents\ProcessoSeletivo_IntuitiveCare\Teste3' 
-into table relatorio_cadop2
-FIELDS TERMINATED BY ';' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+load data local infile 'C:/Users/Gabriela/Documents/ProcessoSeletivo_IntuitiveCare/Teste3/Dados2021/4T2021/4T2021.csv'
+into table relatorio_cadop
+character set latin1
+fields terminated by ';' 
+optionally enclosed by '"'
+lines terminated by '\r\n'
+ignore 1 lines;
